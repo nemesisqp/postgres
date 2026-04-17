743565716a1f7c11fec87807fdcbb7d76167c2ff
NĐQP <nemesisqp@users.noreply.github.com>
2026-04-18 02:37:06 +0700

    fix pgbackrest v15

--- initdb-pgbackrest.sh
+++ initdb-pgbackrest.sh
@@ -9,3 +9,11 @@ echo "wal_level = replica" >> "$PGDATA/postgresql.auto.conf"
 echo "archive_timeout = 1800s" >> "$PGDATA/postgresql.auto.conf"
 echo "archive_command = 'pgbackrest --stanza=main archive-push %p'" >> "$PGDATA/postgresql.auto.conf"
 echo "Archive enabled"
+
+until pg_isready -U postgres; do
+  sleep 1
+done
+
+echo "Creating pgBackRest stanza 'main'..."
+pgbackrest stanza-create --stanza=main --log-level-stderr=info
+echo "pgBackRest stanza created successfully."
